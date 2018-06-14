40cbcccff34e16f8f10f73f36207f8985137146c
Isaac Wong <isaac@thirdcaptain.com>
2018-06-14 03:12:52 +0000

    init deleted and amenity_ids fixed

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -39,7 +39,6 @@ class TestFileStorageMethods(unittest.TestCase):
     def test_doc(self):
         """testing docstrings"""
         self.assertIsNotNone(FileStorage.__doc__)
-        self.assertIsNotNone(FileStorage.__init__.__doc__)
         self.assertIsNotNone(FileStorage.all.__doc__)
         self.assertIsNotNone(FileStorage.save.__doc__)
         self.assertIsNotNone(FileStorage.new.__doc__)
